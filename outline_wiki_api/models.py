7dca854803c9c7859db1626f89fd1a2a34f86ee1
eppv <evgeniypalych@gmail.com>
2025-03-29 22:08:09 +1000

    Improve models validation
    Add pydantic[email] dependency
    Add aliases to camelCase fields

--- outline_wiki_api/models.py
+++ outline_wiki_api/models.py
@@ -1,42 +1,42 @@
 
 from datetime import datetime
 from typing import Optional, List, Dict, Any
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
 
 
 class User(BaseModel):
     id: str
     name: str
-    avatarUrl: Optional[str]
-    email: Optional[str]
+    avatar_url: Optional[str] = Field(..., alias='avatarUrl')
+    email: Optional[EmailStr]
     color: Optional[str]
     role: Optional[str]
-    isSuspended: bool
-    createdAt: datetime
-    updatedAt: datetime
-    lastActiveAt: Optional[datetime]
+    is_suspended: bool = Field(..., alias='isSuspended')
+    created_at: datetime = Field(..., alias='createdAt')
+    updated_at: datetime = Field(..., alias='updatedAt')
+    last_active_at: Optional[datetime] = Field(..., alias='lastActiveAt')
     timezone: Optional[str]
     language: Optional[str]
     preferences: Optional[Dict]
-    notificationSettings: Optional[Dict]
+    notification_settings: Optional[Dict] = Field(..., alias='notificationSettings')
 
 
 class Team(BaseModel):
     id: str
     name: str
-    avatarUrl: Optional[str]
+    avatar_url: Optional[str] = Field(..., alias='avatarUrl')
     sharing: bool
-    memberCollectionCreate: bool
-    memberTeamCreate: bool
-    defaultCollectionId: Optional[str]
-    documentEmbeds: bool
-    guestSignin: bool
+    member_collection_create: bool = Field(..., alias='memberCollectionCreate')
+    member_team_create: bool = Field(..., alias='memberTeamCreate')
+    default_collection_id: Optional[str] = Field(..., alias='defaultCollectionId')
+    document_embeds: bool = Field(..., alias='documentEmbeds')
+    guest_sign_in: bool = Field(..., alias='guestSignin')
     subdomain: Optional[str]
     domain: Optional[str]
     url: str
-    defaultUserRole: str
-    inviteRequired: bool
-    allowedDomains: Optional[List]
+    defaultUserRole: str = Field(..., alias='defaultUserRole')
+    invite_required: bool = Field(..., alias='inviteRequired')
+    allowed_domains: Optional[List] = Field(..., alias='allowedDomains')
     preferences: Optional[Dict]
 
 
@@ -45,23 +45,23 @@ class Collection(BaseModel):
     name: str
     description: Optional[str]
     private: bool
-    created_at: datetime
-    updated_at: datetime
-    deleted_at: Optional[datetime]
+    created_at: datetime = Field(..., alias='createdAt')
+    updated_at: datetime = Field(..., alias='updatedAt')
+    deleted_at: Optional[datetime] = Field(..., alias='deletedAt')
     documents: Optional[List[Dict[str, Any]]]
 
 
 class Document(BaseModel):
     id: str
-    collection_id: Optional[str]
+    collection_id: Optional[str] = Field(..., alias='collectionId')
     title: str
     text: str
     url: str
     emoji: Optional[str]
     pinned: bool
-    created_at: datetime
-    updated_at: datetime
-    published_at: Optional[datetime]
+    created_at: datetime = Field(..., alias='createdAt')
+    updated_at: datetime = Field(..., alias='updatedAt')
+    published_at: Optional[datetime] = Field(..., alias='publishedAt')
     last_modified_by: Optional[User]
     created_by: User
     updated_by: User
