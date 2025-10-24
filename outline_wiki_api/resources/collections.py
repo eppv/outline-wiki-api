e0107fe78bb9476e2f5f56cdceab8d2486427bad
eppv <evgeniypalych@gmail.com>
2025-10-25 00:28:09 +1000

    Some refactoring

--- outline_wiki_api/resources/collections.py
+++ outline_wiki_api/resources/collections.py
@@ -1,9 +1,11 @@
-
-from typing import Optional, List, Dict, Union
 from uuid import UUID
 from .base import Resources
-from ..models.response import Pagination, Sort
-from ..models.collection import CollectionResponse, CollectionListResponse, CollectionNavigationResponse
+from ..models.response import Pagination, Permission, Sort
+from ..models.collection import (
+    CollectionResponse,
+    CollectionListResponse,
+    CollectionNavigationResponse,
+)
 
 
 class Collections(Resources):
@@ -19,9 +21,10 @@ class Collections(Resources):
         list: List all collections
 
     """
-    _path: str = '/collections'
 
-    def info(self, collection_id: Union[str, UUID]) -> CollectionResponse:
+    _path: str = "/collections"
+
+    def info(self, collection_id: str | UUID) -> CollectionResponse:
         """
         Retrieve a collection
 
@@ -37,7 +40,7 @@ class Collections(Resources):
         response = self.post("info", data=data)
         return CollectionResponse(**response.json())
 
-    def documents(self, collection_id: Union[str, UUID]):
+    def documents(self, collection_id: str | UUID):
         """
         Retrieve a collections document structure (as nested navigation nodes)
 
@@ -53,11 +56,11 @@ class Collections(Resources):
         return CollectionNavigationResponse(**response.json())
 
     def list(
-            self,
-            query: Optional[str] = None,
-            status_filter: Optional[List[str]] = None,
-            pagination: Optional[Pagination] = None,
-            sorting: Optional[Sort] = None
+        self,
+        query: str | None = None,
+        status_filter: list[str] | None = None,
+        pagination: Pagination | None = None,
+        sorting: Sort | None = None,
     ) -> CollectionListResponse:
         """
         List all collections
@@ -77,10 +80,50 @@ class Collections(Resources):
         if status_filter:
             data["statusFilter"] = status_filter
         if pagination:
-            data.update(pagination.dict())
+            data.update(pagination)
         if sorting:
-            data.update(sorting.dict())
+            data.update(sorting)
 
         response = self.post("list", data=data)
 
         return CollectionListResponse(**response.json())
+
+    def create(
+        self,
+        name: str,
+        description: str | None,
+        permission: Permission | None,
+        icon: str | None,
+        color: str | None,
+        sharing: bool | None,
+    ):
+        """
+        Create a new collection
+
+        Args:
+            name: The name of the collection
+            description: The description of the collection
+            permission: The permission of the collection
+            icon: The icon of the collection
+            color: The color of the collection
+            sharing: Whether the collection is shared
+
+        Returns:
+            Collection: The created collection
+        """
+
+        data = {}
+        if description:
+            data["description"] = description
+        if permission:
+            data["permission"] = permission
+        if icon:
+            data["icon"] = icon
+        if color:
+            data["color"] = color
+        if sharing:
+            data["sharing"] = sharing
+
+        response = self.post("create", data=data)
+
+        return CollectionResponse(**response.json())
